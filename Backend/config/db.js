594ad98751f0d06de0e8de45f849673a1d6f5424
Kesmi Mumbaiwala <kesmimumbaiwala@gmail.com>
2025-04-14 14:19:41 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://zariSelling:zariSell786@cluster0.aorih.mongodb.net/zarisell?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("db conneccted"))
+    await mongoose.connect('mongodb+srv://zariSellings:zariSell@cluster0.aorih.mongodb.net/zarisell?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("db conneccted"))
 }
-//username:zariSelling
-//pwd:zariSell786
